fd1b5e61719b41404d7b3166cb4b6445fac936c0
AngelPLJ <jponcel1401@alumno.ipn.mx>
2024-01-09 14:13:37 -0600

    YaFunciono

--- php/PruebaAjax.php
+++ php/PruebaAjax.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 // Recibe los datos JSON del cuerpo de la solicitud
 $json_data = file_get_contents('php://input');
 
