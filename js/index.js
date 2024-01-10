ecc21ccf0cdf0db2c62503fda1b97940e3bb1a2a
AngelPLJ <jponcel1401@alumno.ipn.mx>
2024-01-10 17:29:47 -0600

    Muchos cambios al index, ya cambia de noticias

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Realiza una solicitud AJAX para obtener el archivo JSON
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          // Maneja la respuesta JSON
+          var jsonResponse = JSON.parse(xhr.responseText);
+          llenarImagenes(jsonResponse);
+      }
+  };
+
+  xhr.open("GET", "../php/ObtenerDatos.php", true);
+  xhr.send();
+});
+
+function llenarImagenes(data) {
+  // ObtÃ©n el contenedor donde se mostrarÃ¡n los datos
+  var imagen1 = document.getElementById("imagen1");
+  var imagen2 = document.getElementById("imagen2");
+  var imagen3 = document.getElementById("imagen3");
+  var cont = 1;
+
+  // Cambia las imagenes y e texto
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+          switch(cont) {
+            case 1: {
+              imagen1.innerHTML = "<a href='../php/noticia.php?titulo=" + data[key]['tituloNot'] + "'><img src='" + data[key]['imagen'] + "' style='width: 100%;'></a><div class='text'>" + data[key]['tituloNot'] + "</div>";
+            } 
+            case 2: {
+              imagen2.innerHTML = "<a href='../php/noticia.php?titulo=" + data[key]['tituloNot'] + "'><img src='" + data[key]['imagen'] + "' style='width: 100%;'></a><div class='text'>" + data[key]['tituloNot'] + "</div>";
+            } 
+            case 3: {
+              imagen3.innerHTML = "<a href='../php/noticia.php?titulo=" + data[key]['tituloNot'] + "'><img src='" + data[key]['imagen'] + "' style='width: 100%;'></a><div class='text'>" + data[key]['tituloNot'] + "</div>";
+            } 
+          }
+          cont = cont + 1;
+      }
+  }
+}
+
+
 let slideIndex = 0;
 showSlides();
 
--- php/noticia.php
+++ php/noticia.php
@@ -36,6 +36,12 @@ $dato=$stmt->get_result();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../logo/CIDETECH.png">
+    <link rel = "stylesheet" href="../css/indexCss.css"/>
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
     <title>
         <?php 
             echo htmlspecialchars($titulo); 
