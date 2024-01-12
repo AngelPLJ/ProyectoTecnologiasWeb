37bfa984024f1996e755b8195245feabaa96eb67
AngelPLJ <jponcel1401@alumno.ipn.mx>
2024-01-12 17:11:03 -0600

    Terminado

--- php/contacto.php
+++ php/contacto.php
@@ -18,25 +18,21 @@ if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 
+if (isset($_POST['submit'])) {		
+    // Accede a los valores individuales
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $acerca = $_POST['acerca'];
+    $comentario = $_POST['comentario'];
+    $sql= "INSERT INTO Contactos (nombre, email, acerca, comentario) VALUES ('$nombre', '$email', '$acerca', '$comentario')";
+	
+    if($conexion->query($sql)) {
+        $success_message = "Edited Successfully";
+    } else {
+        $error_message = "Problem in Editing Record";
+    }
+    header("Location: ../html");
 
-// Recibe los datos JSON del cuerpo de la solicitud
-$json_data = file_get_contents('php://input');
-
-// Decodifica los datos JSON a un array de PHP
-$data = json_decode($json_data, true);
-
-// Accede a los valores individuales
-$nombre = $data['nombre'];
-$email = $data['email'];
-$acerca = $data['acerca'];
-$comentario = $data['comentario'];
-$sql= "INSERT INTO Contactos (nombre, email, acerca, comentario) VALUES ('$nombre', '$email', '$acerca', '$comentario')";
-
-if ($conexion->query($sql) === TRUE) {
-    echo json_encode(['status' => 'success', 'message' => 'Datos recibidos con éxito']);
-} else {
-    echo json_encode(['status' => 'failed', 'message' => 'Error']);
 }
-
 $conexion->close();
 ?>
