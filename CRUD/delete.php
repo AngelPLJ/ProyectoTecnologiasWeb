76bfe9a6308f476fd04f801ffd24fcc246ae0821
AngelPLJ <jponcel1401@alumno.ipn.mx>
2024-01-12 19:12:40 -0600

    Corregido el Delete

--- CRUD/delete.php
+++ CRUD/delete.php
@@ -1,6 +1,10 @@
 <?php 
     require_once("db.php");
 	
+	$sql = $conn->prepare("DELETE  FROM Usuarios WHERE Noticia=?");  
+	$sql->bind_param("i", $_GET["id"]); 
+	$sql->execute();
+
 	$sql = $conn->prepare("DELETE  FROM Noticias WHERE id=?");  
 	$sql->bind_param("i", $_GET["id"]); 
 	$sql->execute();
